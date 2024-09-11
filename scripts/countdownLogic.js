5214a538bbfbcaa3379db997b2a0ae5bad9f1080
dauntss <kat.krahulec@gmail.com>
2024-09-10 22:13:43 -0600

    ACTUALLY fixing the thing I broke

--- scripts/countdownLogic.js
+++ scripts/countdownLogic.js
@@ -4,11 +4,11 @@ let games = [
 
     { name: "Indiana Jones and the Great Circle", filter: ["Console", "Pc", "Single Player"], genre: ["Action", "Adventure"], releaseDate: new Date("December 8, 2024 23:59:59"), imageUrl: "https://assets-prd.ignimgs.com/2024/01/18/indianajonesgreatcircle-1705613822118.jpg?width=300&crop=1%3A1%2Csmart&auto=webp&dpr=2" },
 
-    { name: "Europa", filter: "Pc", genre: "Adventure", releaseDate: new Date("October 10, 2024 23:59:59"), imageUrl: "https://assets-prd.ignimgs.com/2022/12/07/europa-button-02-1670376231449.jpg?width=300&crop=1%3A1%2Csmart&auto=webp&dpr=2" },
+    { name: "Europa", filter: ["Console", "Pc", "Single Player"], genre: ["Adventure"], releaseDate: new Date("October 10, 2024 23:59:59"), imageUrl: "https://assets-prd.ignimgs.com/2022/12/07/europa-button-02-1670376231449.jpg?width=300&crop=1%3A1%2Csmart&auto=webp&dpr=2" },
 
     { name: "Phoenix Springs", filter: ["Pc", "Single Player"], genre: ["Adventure"], releaseDate: new Date("December 31, 2024 23:59:59"), imageUrl: "https://assets-prd.ignimgs.com/2024/06/08/untitled-1-1717866642448.png?width=300&crop=1%3A1%2Csmart&auto=webp&dpr=2" },
 
-    { name: "Undisputed", filter: "Multi Player", genre: "Sports", releaseDate: new Date("October 10, 2024 23:59:59"), imageUrl: "https://assets-prd.ignimgs.com/2022/10/11/undisputed-button-2-1665519933271.jpg?width=300&crop=1%3A1%2Csmart&auto=webp&dpr=2" },
+    { name: "Undisputed", filter: ["Console", "Pc", "Multi Player"], genre: ["Sports"], releaseDate: new Date("October 10, 2024 23:59:59"), imageUrl: "https://assets-prd.ignimgs.com/2022/10/11/undisputed-button-2-1665519933271.jpg?width=300&crop=1%3A1%2Csmart&auto=webp&dpr=2" },
 
     { name: "NHL 25", filter: ["Console", "Multi Player"], genre: ["Sports"], releaseDate: new Date("September 26, 2024 23:59:59"), imageUrl: "https://assets-prd.ignimgs.com/2024/08/22/nhl-25-button-1724331930889.jpg?width=300&crop=1%3A1%2Csmart&auto=webp&dpr=2" },
 
@@ -51,8 +51,8 @@ function createTimerElements() {
         `
             <h2 class="text-2xl font-bold mb-4">${game.name}</h2>
             <p id="${game.name.replace(/\s+/g, '')}" class="text-2xl font-bold"></p>
-            <p class="text-sm text-gray-400">Availability: ${game.filter.join(',&nbsp')}</p>
-            <p class="text-sm text-gray-400">Genre: ${game.genre.join(',&nbsp')}</p>
+            <p class="text-sm text-gray-600">Availability: ${game.filter.join(',&nbsp')}</p>
+            <p class="text-sm text-gray-600">Genre: ${game.genre.join(',&nbsp')}</p>
             
         `;
         timersDiv.appendChild(timerDiv);
